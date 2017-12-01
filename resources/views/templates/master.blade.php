6a6e965627a2b43b16dfeef68840cbac473d8baa
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-12-01 12:23:00 -0200

    Imported menu view in master template

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <title>Investe Bem | @yield('page-title')</title>
     <link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fredoka+One">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     @yield('css-view')
 </head>
 <body>
+    @include('templates.menu')
     @yield('content-view')
     @yield('js-view')
 </body>
