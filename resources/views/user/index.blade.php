1d3b5e255eff7624ad25aa74fb7cbf81817dbb7f
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-04 19:07:55 -0200

    Adjusted name id

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -13,7 +13,7 @@
 @section('content-view')
     {!! Form::open(['route' => 'user.store','method' => 'post', 'class' => 'form-default']) !!}
     @include('form.input', ['input' => 'cpf', 'attributes' => ['placeholder' => 'CPF']])
-    @include('form.input', ['input' => '', 'attributes' => ['placeholder' => 'Nome']])
+    @include('form.input', ['input' => 'name', 'attributes' => ['placeholder' => 'Nome']])
     @include('form.input', ['input' => 'phone', 'attributes' => ['placeholder' => 'Telefone']])
     @include('form.input', ['input' => 'email', 'attributes' => ['placeholder' => 'E-mail']])
     @include('form.password', ['input' => 'password', 'attributes' => ['placeholder' => 'Senha']])
