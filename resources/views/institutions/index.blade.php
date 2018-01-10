fc9e2204dfbd80d4848784521953b5ef4dd028a6
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-10 12:23:12 -0200

    Created institution view

--- resources/views/institutions/index.blade.php
+++ resources/views/institutions/index.blade.php
@@ -2,4 +2,36 @@
 
 @section('content-view')
 
+    @if(session('success'))
+        <h3>{{ session('success')['messages'] }}</h3>
+    @endif
+
+    {!! Form::open(['route' => 'institution.store', 'method' => 'POST', 'class' => 'form-default']) !!}
+    @include('form.input', ['input' => 'name', 'attributes' => ['placeholder' => 'Nome']])
+    @include('form.submit', ['input' => 'Cadastrar'])
+    {!! Form::close() !!}
+
+    <table class="default-table">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Ações</th>
+        </thead>
+        <tbody>
+        @foreach($institutions as $institution)
+        <tr>
+            <td>{{ $institution->id }}</td>
+            <td>{{ $institution->name }}</td>
+            <td>
+                {!! Form::open(['route' => ['institution.destroy', $institution->id], 'method' => 'DELETE']); !!}
+                {!! Form::submit('Remover') !!}
+                {!! Form::close(); !!}
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+        </tr>
+    </table>
+
 @endsection
