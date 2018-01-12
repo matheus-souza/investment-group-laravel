483ef7116654220993bd809d0828bdd0a0a8cdef
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-12 12:33:09 -0200

    Table created to list groups

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -15,7 +15,32 @@
     @include('form.submit', ['input' => 'Cadastrar'])
     {!! Form::close() !!}
 
-
+    <table class="default-table">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Nome do grupo</th>
+            <th>Instituição</th>
+            <th>Nome do responsável</th>
+            <th>Ações</th>
+        </thead>
+        <tbody>
+        @foreach($groups as $group)
+            <tr>
+                <td>{{ $group->id }}</td>
+                <td>{{ $group->name }}</td>
+                <td>{{ $group->institution->name }}</td>
+                <td>{{ $group->user->name }}</td>
+                <td>
+                    {!! Form::open(['route' => ['group.destroy', $group->id], 'method' => 'DELETE']); !!}
+                    {!! Form::submit('Remover') !!}
+                    {!! Form::close(); !!}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+        </tr>
+    </table>
 
 
 @endsection
