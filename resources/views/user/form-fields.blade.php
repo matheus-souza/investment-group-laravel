f6007e5bede179596c5cbad9d9d0638d7bfcbea6
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-16 12:57:13 -0200

    Decoupled user inputs

---
+++ resources/views/user/form-fields.blade.php
@@ -0,0 +1,5 @@
+@include('form.input', ['input' => 'cpf', 'attributes' => ['placeholder' => 'CPF']])
+@include('form.input', ['input' => 'name', 'attributes' => ['placeholder' => 'Nome']])
+@include('form.input', ['input' => 'phone', 'attributes' => ['placeholder' => 'Telefone']])
+@include('form.input', ['input' => 'email', 'attributes' => ['placeholder' => 'E-mail']])
+@include('form.password', ['input' => 'password', 'attributes' => ['placeholder' => 'Senha']])
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -17,11 +17,7 @@
     @endif
 
     {!! Form::open(['route' => 'user.store','method' => 'post', 'class' => 'form-default']) !!}
-    @include('form.input', ['input' => 'cpf', 'attributes' => ['placeholder' => 'CPF']])
-    @include('form.input', ['input' => 'name', 'attributes' => ['placeholder' => 'Nome']])
-    @include('form.input', ['input' => 'phone', 'attributes' => ['placeholder' => 'Telefone']])
-    @include('form.input', ['input' => 'email', 'attributes' => ['placeholder' => 'E-mail']])
-    @include('form.password', ['input' => 'password', 'attributes' => ['placeholder' => 'Senha']])
+    @include('user.form-fields')
     @include('form.submit', ['input' => 'Cadastrar'])
     {!! Form::close() !!}
 
