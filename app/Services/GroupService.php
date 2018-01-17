e24770260a54ff8636a19aff9a4f31c75a435628
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-16 22:25:29 -0200

    Created update method

--- app/Services/GroupService.php
+++ app/Services/GroupService.php
@@ -72,4 +72,29 @@ class GroupService
             }
         }
     }
+
+    public function update($data, $id)
+    {
+        try {
+        $this->validator->with($data)->passesOrFail(ValidatorInterface::RULE_UPDATE);
+        $group = $this->repository->update($data, $id);
+
+        return [
+            'success' => true,
+            'messages' => 'Grupo atualizado',
+            'data' => $group,
+        ];
+    } catch (Exception $e) {
+        switch (get_class($e)) {
+            case QueryException::class:
+                return ['success' => false, 'messages' => 'Erro de execução: ' . $e->getMessage(), 'class:' => get_class($e)];
+            case ValidatorException::class:
+                return ['success' => false, 'messages' => 'Erro de execução: ' . $e->getMessageBag(), 'class:' => get_class($e)];
+            case Exception::class:
+                return ['success' => false, 'messages' => 'Erro de execução: ' . $e->getMessage(), 'class:' => get_class($e)];
+            default:
+                return ['success' => false, 'messages' => 'Erro de execução: ' . $e->getMessage(), 'class:' => get_class($e)];
+        }
+    }
+    }
 }
