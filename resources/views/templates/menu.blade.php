27c016e79ec016a6c38acb4bb18503fd27bb26cf
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-22 21:33:27 -0200

    Migrated menu to Bootstrap 4

--- resources/views/templates/menu.blade.php
+++ resources/views/templates/menu.blade.php
@@ -1,22 +1,20 @@
-<nav id="main">
-    <ul>
-        <li>
-            <a href="{{ route('user.index') }}">
-                <i class="fa fa-address-book"></i>
-                <h3>Usuários</h3>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('institution.index') }}">
-                <i class="fa fa-building"></i>
-                <h3>Instituições</h3>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('group.index') }}">
-                <i class="fa fa-users"></i>
-                <h3>Grupos</h3>
-            </a>
-        </li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Investe Bem</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('user.index') }}"><i class="fa fa-address-book"></i> Usuários</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('institution.index') }}"><i class="fa fa-building"></i> Instituições</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('group.index') }}"><i class="fa fa-users"></i> Grupos</a>
+            </li>
+        </ul>
+    </div>
 </nav>
