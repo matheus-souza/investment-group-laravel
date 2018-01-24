057796e974d988ed9d995c8ae458e58281ff9975
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-23 23:33:17 -0200

    Adjacent classes

--- resources/views/form/input.blade.php
+++ resources/views/form/input.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-6 mb-3">
+<div class="form-group col-md-6">
     {!! Form::label($input, $attributes['placeholder'] ?? $input, ['class' => 'control-label']) !!}
     {!! Form::text($input, $value ?? null, array_merge(['class' => 'form-control'], $attributes)) !!}
 </div>
