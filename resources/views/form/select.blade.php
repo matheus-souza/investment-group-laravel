6f05143e355f6c7fd3734b64783088779db36403
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-23 23:06:24 -0200

    Migrated select to Bootstrap 4

--- resources/views/form/select.blade.php
+++ resources/views/form/select.blade.php
@@ -1,4 +1,4 @@
-<label class="{{ $class ?? null }}">
-    <span>{{ $label ?? $select }}</span>
-    {!! Form::select($select, $data ?? []) !!}
-</label>
+<div class="col-md-6 mb-3">
+    <label for="{{ $select }}">{{ $label ?? $select}}</label>
+    {!! Form::select($select, $data ?? [], null, ['class' => 'form-control']) !!}
+</div>
