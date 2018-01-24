a24c2b4bae9e804673e16d47e1732ce13e61b88b
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-23 22:50:50 -0200

    Migrated password to Bootstrap 4

--- resources/views/form/password.blade.php
+++ resources/views/form/password.blade.php
@@ -1,4 +1,4 @@
-<label class="{{ $class ?? null }}">
-    <span>{{ $label ?? $input }}</span>
-    {!! Form::password($input, $attributes) !!}
-</label>
+<div class="col-md-6 mb-3">
+    {!! Form::label($input, $attributes['placeholder'] ?? $input, ['class' => 'control-label']) !!}
+    {!! Form::password($input, array_merge(['class' => 'form-control'], $attributes)) !!}
+</div>
