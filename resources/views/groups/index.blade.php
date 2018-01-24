95d88521f8c7e4797aab46be7c176372cfd51784
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-24 12:32:11 -0200

    Group view refactoring

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -6,14 +6,18 @@
 
 @section('content-view')
 
-    {!! Form::open(['route' => 'group.store', 'method' => 'POST', 'class' => 'form-default']) !!}
-    @include('form.input', ['input' => 'name', 'attributes' => ['placeholder' => 'Nome']])
-
-    @include('form.select', ['select' => 'user_id', 'data' => $user_list,'attributes' => ['placeholder' => 'User ID']])
-    @include('form.select', ['select' => 'institution_id', 'data' => $institution_list,'attributes' => ['placeholder' => 'Institution ID']])
-
-    @include('form.submit', ['input' => 'Cadastrar'])
-    {!! Form::close() !!}
+    <div class="form-control">
+        {!! Form::open(['route' => 'group.store', 'method' => 'POST', 'class' => 'form-default']) !!}
+        <div class="form-row">
+            @include('form.input', ['input' => 'name', 'attributes' => ['placeholder' => 'Nome']])
+            @include('form.select', ['select' => 'user_id', 'label' => 'Usuário', 'data' => $user_list,'attributes' => ['placeholder' => 'User ID']])
+        </div>
+        <div class="form-row">
+            @include('form.select', ['select' => 'institution_id', 'label' => 'Instituição', 'data' => $institution_list,'attributes' => ['placeholder' => 'Institution ID']])
+        </div>
+        @include('form.submit', ['input' => 'Cadastrar'])
+        {!! Form::close() !!}
+    </div>
 
     @include('groups.list', ['group_list' => $groups])
 
