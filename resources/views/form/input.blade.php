7e7b984a80a17717e29328466a1d3d775ee52eb4
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-23 22:27:22 -0200

    Migrated input to Bootstrap 4

--- resources/views/form/input.blade.php
+++ resources/views/form/input.blade.php
@@ -1,4 +1,4 @@
-<label class="{{ $class ?? null }}">
-    <span>{{ $label ?? $input }}</span>
-    {!! Form::text($input, $value ?? null, $attributes) !!}
-</label>
+<div class="col-md-6 mb-3">
+    {!! Form::label($input, $attributes['placeholder'] ?? $input, ['class' => 'control-label']) !!}
+    {!! Form::text($input, $value ?? null, array_merge(['class' => 'form-control'], $attributes)) !!}
+</div>
