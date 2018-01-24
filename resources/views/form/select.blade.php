b75fdaf8f34518fa13cd8eeaafb4fa707375518a
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-24 12:33:10 -0200

    Adjacent classes in select

--- resources/views/form/select.blade.php
+++ resources/views/form/select.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-6 mb-3">
+<div class="form-group col-md-6">
     <label for="{{ $select }}">{{ $label ?? $select}}</label>
     {!! Form::select($select, $data ?? [], null, ['class' => 'form-control']) !!}
 </div>
