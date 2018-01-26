d95dd1d4d4e386f07b77f7b7ffc2ad5cc0619005
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-25 23:06:06 -0200

    Migrated user table for Bootstrap 4

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -1,21 +1,21 @@
-<table class="default-table">
+<table class="table table-hover">
     <thead>
-    <tr>
-        <td>#</td>
-        <td>CPF</td>
-        <td>Nome</td>
-        <td>Telefone</td>
-        <td>Nascimento</td>
-        <td>E-mail</td>
-        <td>Status</td>
-        <td>Permissão</td>
-        <td>Ações</td>
-    </tr>
+        <tr>
+            <th scope="col">#</th >
+            <th scope="col">CPF</th >
+            <th scope="col">Nome</th >
+            <th scope="col">Telefone</th >
+            <th scope="col">Nascimento</th >
+            <th scope="col">E-mail</th >
+            <th scope="col">Status</th >
+            <th scope="col">Permissão</th >
+            <th scope="col">Ações</th >
+        </tr>
     </thead>
     <tbody>
     @foreach($user_list as $user)
         <tr>
-            <td>{{ $user->id }}</td>
+            <th scope="row">{{ $user->id }}</th>
             <td>{{ $user->formatted_cpf }}</td>
             <td>{{ $user->name }}</td>
             <td>{{ $user->formatted_phone }}</td>
@@ -23,11 +23,15 @@
             <td>{{ $user->email }}</td>
             <td>{{ $user->status }}</td>
             <td>{{ $user->permission }}</td>
-            <td>
-                {!! Form::open(['route' => ['user.destroy', $user->id], 'method' => 'DELETE']) !!}
-                {!! Form::submit('Remover') !!}
-                {!! Form::close() !!}
-                <a href="{{ route('user.edit', $user->id) }}">Editar</a>
+            <td class="btn-toolbar">
+                <div class="btn-group mr-2">
+                    <a href="{{ route('user.edit', $user->id) }}" class="btn btn-xs btn-warning">Editar</a>
+                </div>
+                <div class="btn-group mr-2">
+                    {!! Form::open(['route' => ['user.destroy', $user->id], 'method' => 'DELETE']) !!}
+                    <button type="submit" class="btn btn-danger">Remover</button>
+                    {!! Form::close() !!}
+                </div>
             </td>
         </tr>
     @endforeach
