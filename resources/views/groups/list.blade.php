8fc6304d8bd85a30cd9b621fee9f37fe4a961db1
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-25 23:22:50 -0200

    Migrated group table for Bootstrap 4

--- resources/views/groups/list.blade.php
+++ resources/views/groups/list.blade.php
@@ -1,25 +1,32 @@
-<table class="default-table">
+<table class="table table-hover">
     <thead>
-    <tr>
-        <th>#</th>
-        <th>Nome do grupo</th>
-        <th>Instituição</th>
-        <th>Nome do responsável</th>
-        <th>Ações</th>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome do grupo</th>
+            <th scope="col">Instituição</th>
+            <th scope="col">Nome do responsável</th>
+            <th scope="col">Ações</th>
+        </tr>
     </thead>
     <tbody>
     @foreach($group_list as $group)
         <tr>
-            <td>{{ $group->id }}</td>
+            <th scope="row">{{ $group->id }}</th>
             <td>{{ $group->name }}</td>
             <td>{{ $group->institution->name }}</td>
             <td>{{ $group->user->name }}</td>
-            <td>
+            <td class="btn-toolbar">
+                <div class="btn-group mr-2">
+                <a href="{{ route('group.show', $group->id) }}" class="btn btn-xs btn-info">Detalhes</a>
+                </div>
+                <div class="btn-group mr-2">
+                <a href="{{ route('group.edit', $group->id) }}" class="btn btn-xs btn-warning">Editar</a>
+                </div>
+                <div class="btn-group mr-2">
                 {!! Form::open(['route' => ['group.destroy', $group->id], 'method' => 'DELETE']); !!}
-                {!! Form::submit('Remover') !!}
+                    <button type="submit" class="btn btn-danger">Remover</button>
                 {!! Form::close(); !!}
-                <a href="{{ route('group.show', $group->id) }}">Detalhes</a>
-                <a href="{{ route('group.edit', $group->id) }}">Editar</a>
+                </div>
             </td>
         </tr>
     @endforeach
