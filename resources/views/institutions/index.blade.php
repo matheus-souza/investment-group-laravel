8ad7c30da7c2f7c177f2696d4480f5c60400e51b
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-25 23:12:23 -0200

    Migrated institution table for Bootstrap 4

--- resources/views/institutions/index.blade.php
+++ resources/views/institutions/index.blade.php
@@ -19,25 +19,34 @@
         {!! Form::close() !!}
     </div>
 
-    <table class="default-table">
+    <table class="table table-hover">
         <thead>
-        <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Ações</th>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Ações</th>
+            </tr>
         </thead>
         <tbody>
         @foreach($institutions as $institution)
         <tr>
-            <td>{{ $institution->id }}</td>
+            <th scope="row">{{ $institution->id }}</th>
             <td>{{ $institution->name }}</td>
-            <td>
-                {!! Form::open(['route' => ['institution.destroy', $institution->id], 'method' => 'DELETE']); !!}
-                {!! Form::submit('Remover') !!}
-                {!! Form::close(); !!}
-                <a href="{{ route('institution.show', $institution->id) }}">Detalhes</a>
-                <a href="{{ route('institution.edit', $institution->id) }}">Editar</a>
-                <a href="{{ route('institution.product.index', $institution->id) }}">Produtos</a>
+            <td class="btn-toolbar">
+                <div class="btn-group mr-2">
+                    <a href="{{ route('institution.product.index', $institution->id) }}" class="btn btn-xs btn-secondary">Produtos</a>
+                </div>
+                <div class="btn-group mr-2">
+                    <a href="{{ route('institution.show', $institution->id) }}" class="btn btn-xs btn-info">Detalhes</a>
+                </div>
+                <div class="btn-group mr-2">
+                    <a href="{{ route('institution.edit', $institution->id) }}" class="btn btn-xs btn-warning">Editar</a>
+                </div>
+                <div class="btn-group mr-2">
+                    {!! Form::open(['route' => ['institution.destroy', $institution->id], 'method' => 'DELETE']); !!}
+                    <button type="submit" class="btn btn-danger">Remover</button>
+                    {!! Form::close(); !!}
+                </div>
             </td>
         </tr>
         @endforeach
