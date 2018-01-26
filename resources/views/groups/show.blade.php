51790ca4a1bfe1707aa78a9cf01eae9bc9adccf0
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-01-26 12:57:14 -0200

    Group show view refactoring

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -5,16 +5,19 @@
 @endsection
 
 @section('content-view')
-    <header>
-        <h1>Nome do grupo: {{ $group->name }}</h1>
-        <h2>Instituição: {{ $group->institution->name }}</h2>
-        <h2>Responsável: {{ $group->user->name    }}</h2>
-    </header>
 
-    {!! Form::open(['route' => ['group.user.store', $group->id], 'method' => 'POST', 'class' => 'form-default']) !!}
-    @include('form.select', ['select' => 'user_id', 'data' => $user_list, 'attributes' => ['placeholder' => 'Usuário']])
-    @include('form.submit', ['input' => 'Relacionar ao Grupo ' . $group->name])
-    {{ Form::close() }}
+    <div class="form-control">
 
+        <h3>Nome do grupo: {{ $group->name }}</h3>
+        <h4>Instituição: {{ $group->institution->name }}</h4>
+        <h4>Responsável: {{ $group->user->name    }}</h4>
+
+        {!! Form::open(['route' => ['group.user.store', $group->id], 'method' => 'POST']) !!}
+        <div class="form-row">
+            @include('form.select', ['select' => 'user_id', 'label' => 'Usuário', 'data' => $user_list])
+        </div>
+        @include('form.submit', ['input' => 'Relacionar ao Grupo ' . $group->name])
+        {{ Form::close() }}
+    </div>
     @include('user.list', ['user_list' => $group->users])
 @endsection
